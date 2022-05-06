6487b855bf900affd59eb51f35f04bb50b968b69
alejo9424 <alejandro.castaneda@payco.co>
2022-05-06 11:50:27 -0500

    Cambios

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-use App\Http\Controllers\AsignaturaController;
-use App\Http\Controllers\CapituloController;
-use App\Http\Controllers\CicloController;
-use App\Http\Controllers\CondicionController;
-use App\Http\Controllers\FacultadController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProgramaAcademicoController;
 use App\Http\Controllers\UserController;
-use App\Http\Middleware\JwtMiddleware;
-use App\Http\Middleware\LogMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,74 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('api')->group(function () {
-
-    Route::get('/ping', function () {
-        return env('APP_NAME');
-    });
-
-
-    Route::post('/login', [LoginController::class, 'login']);
-
-    Route::middleware([LogMiddleware::class, JwtMiddleware::class])->group(function () {
-        Route::get('/logout', [LoginController::class, 'logOut']);
-        Route::prefix('user')->group(function () {
-            Route::post('/create', [UserController::class, 'create']);
-            Route::put('/update', [UserController::class, 'update']);
-            Route::delete('/delete/{id}', [UserController::class, 'delete']);
-            Route::get('/get/{id}', [UserController::class, 'getUser']);
-            Route::get('/list', [UserController::class, 'list']);
-            Route::get('/tipoPersonas', [UserController::class, 'getTiposUsuario']);
-        });
-
-        Route::prefix('facultad')->group(function () {
-            Route::post('/create', [FacultadController::class, 'create']);
-            Route::put('/update', [FacultadController::class, 'update']);
-            Route::delete('/delete/{id}', [FacultadController::class, 'delete']);
-            Route::get('/decanos', [FacultadController::class, 'getDecanos']);
-            Route::get('/get/{id}', [FacultadController::class, 'getFacultad']);
-            Route::get('/list', [FacultadController::class, 'list']);
-        });
-
-        Route::prefix('ciclo')->group(function () {
-            Route::post('/create', [CicloController::class, 'create']);
-            Route::put('/update', [CicloController::class, 'update']);
-            Route::delete('/delete/{id}', [CicloController::class, 'delete']);
-            Route::get('/get/{id}', [CicloController::class, 'getCiclo']);
-            Route::get('/list', [CicloController::class, 'list']);
-        });
-
-        Route::prefix('programa')->group(function () {
-            Route::post('/create', [ProgramaAcademicoController::class, 'create']);
-            Route::put('/update', [ProgramaAcademicoController::class, 'update']);
-            Route::delete('/delete/{id}', [ProgramaAcademicoController::class, 'delete']);
-            Route::get('/get/{id}', [ProgramaAcademicoController::class, 'getPrograma']);
-            Route::get('/list', [ProgramaAcademicoController::class, 'list']);
-            Route::post('/get/condiciones', [ProgramaAcademicoController::class, 'getCondiciones']);
-        });
-
-        Route::prefix('condicion')->group(function () {
-            Route::post('/create', [CondicionController::class, 'create']);
-            Route::put('/update', [CondicionController::class, 'update']);
-            Route::delete('/delete/{id}', [CondicionController::class, 'delete']);
-            Route::get('/get/{id}', [CondicionController::class, 'getCondicion']);
-            Route::get('/list', [CondicionController::class, 'list']);
-        });
-
-        Route::prefix('capitulo')->group(function () {
-            Route::post('/create', [CapituloController::class, 'create']);
-            Route::put('/update', [CapituloController::class, 'update']);
-            Route::delete('/delete/{id}', [CapituloController::class, 'delete']);
-            Route::get('/get/{id}', [CapituloController::class, 'getCapitulo']);
-            Route::get('/list', [CapituloController::class, 'list']);
-        });
-
-        Route::prefix('asignatura')->group(function () {
-            Route::post('/create', [AsignaturaController::class, 'create']);
-            Route::put('/update', [AsignaturaController::class, 'update']);
-            Route::delete('/delete/{id}', [AsignaturaController::class, 'delete']);
-            Route::get('/get/{id}', [AsignaturaController::class, 'getAsignatura']);
-            Route::get('/list', [AsignaturaController::class, 'list']);
-        });
-    });
-});
+Route::get('{any}', function () {
+    return view('app');
+})->where('any', '.*');
